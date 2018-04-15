b6aa598f02f31f880391ee06e7900c1121a8bf94
Hung Do <osxsystem2014@gmail.com>
2018-04-15 21:18:34 +0700

    init api

---
+++ app/Http/Controllers/API/AlbumController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Album;
+use Validator;
+
+class AlbumController extends BaseController
+{
+    public function index()
+    {
+        $albums = Album::all();
+
+        return $this->sendResponse($albums->toArray(), 'Albums retrieved successfully.');
+    }
+
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'title'  		  => 'required',
+            'artist' 		  => 'required',
+            'url'    		  => 'required',
+            'image'           => 'required',
+            'thumbnail_image' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+        $album = Album::create($input);
+
+        return $this->sendResponse($album->toArray(), 'Album created successfully.');
+    }
+
+    public function show($id)
+    {
+        $album = Album::find($id);
+
+        if (is_null($album)) {
+            return $this->sendError('Album not found.');
+        }
+
+        return $this->sendResponse($album->toArray(), 'Album retrieved successfully.');
+    }
+
+    public function update(Request $request, Album $album)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+        $album->title = $input['title'];
+        $album->artist = $input['artist'];
+        $album->artist = $input['url'];
+        $album->artist = $input['image'];
+        $album->artist = $input['thumbnail_image'];
+        $album->save();
+
+        return $this->sendResponse($album->toArray(), 'Album updated successfully.');
+    }
+
+    public function destroy(Album $album)
+    {
+        $album->delete();
+        return $this->sendResponse($album->toArray(), 'Album deleted successfully.');
+    }
+}
---
+++ app/Http/Controllers/Albums.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Albums extends Controller
+{
+    //
+}
